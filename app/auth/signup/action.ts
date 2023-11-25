0e93969f464977192a82e6c956945ba8516342f5
Yo-mah-Ya <shanzhongziye@gmail.com>
2023-11-24 19:18:45 -0800

    add sign-up

---
+++ app/auth/signup/action.ts
@@ -0,0 +1,22 @@
+'use server'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { createClient } from '../supabase'
+import type { SignUpSchema } from './page'
+
+export const signUp = async ({
+  email,
+  password
+}: SignUpSchema): Promise<void> => {
+  const supabase = createClient(cookies())
+
+  const { error } = await supabase.auth.signUp({
+    email,
+    password
+  })
+  if (error) {
+    console.error(error)
+  } else {
+    return redirect('/')
+  }
+}
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,5 +1,47 @@
-import { Heading } from '@chakra-ui/react'
+'use client'
+import { startTransition } from 'react'
+import { useForm } from 'react-hook-form'
+import { Flex, Heading } from '@chakra-ui/react'
+import * as z from 'zod'
+import { PrimaryButton } from '@/components/button'
+import { signUp } from './action'
+
+const signUpSchema = z.object({
+  email: z.string().email(),
+  password: z.string()
+})
+export type SignUpSchema = z.infer<typeof signUpSchema>
 
 export default function SignUp() {
-  return <Heading>SignUp Page</Heading>
+  const { register, handleSubmit } = useForm()
+  return (
+    <>
+      <Heading>SignUp Page</Heading>
+      <form
+        onSubmit={handleSubmit((data) => {
+          const result = signUpSchema.safeParse(data)
+          if (!result.success) {
+            console.error(result.error)
+            return
+          }
+          startTransition(() => {
+            signUp(result.data).catch(console.error)
+          })
+        })}
+      >
+        <Flex flexDirection={'column'}>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" required {...register('email')} />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            {...register('password')}
+          />
+          <PrimaryButton type={'submit'}>Sign Up</PrimaryButton>
+        </Flex>
+      </form>
+    </>
+  )
 }
