aabcbb90cc2d6fc3e8bab3ece241141ae5960c35
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2023-12-03 01:40:57 -0800

    M-6 Activity Create Page

--- app/activity/create/page.tsx
+++ app/activity/create/page.tsx
@@ -1,57 +1,17 @@
 'use client'
-import { useForm } from 'react-hook-form'
+
 import {
   Box,
   Container,
-  Flex,
-  FormLabel,
-  FormErrorMessage,
-  FormControl,
-  Input,
   Heading,
-  useToast,
-  useBoolean,
   useColorModeValue
 } from '@chakra-ui/react'
-import { z } from 'zod'
-import {Form} from "./components"
 import { Header, Footer } from '@/components/navigation'
+import {Form} from "./components"
 
-// const dummy = [
-//   {
-//     id: 1,
-//     title: 'Tokyo',
-//     date_from: 'Oct 10, 2023',
-//     date_to: 'Nov 10, 2023',
-//     image_storage_object_id:
-//       'https://placehold.jp/6381f8/ffffff/366x277.png?text=Picture',
-//     share: [
-//       { id: '', image: '' },
-//       { id: '', image: '' }
-//     ]
-//   },
-//   {
-//     id: 2,
-//     title: 'Kyoto',
-//     date_from: 'Oct 10, 2023',
-//     date_to: 'Nov 10, 2023',
-//     image_storage_object_id:
-//       'https://placehold.jp/6381f8/ffffff/366x277.png?text=Picture',
-//     share: [
-//       { id: '', image: '' },
-//       { id: '', image: '' }
-//     ]
-//   }
-// ]
 
 export default function Create() {
 
-    //define how z schema look like 
-
-    const schema = z.object({
-        firstName: z.string().min(2).max(20),
-        lastName: z.string().min(2).max(20),
-    })
   const bg = useColorModeValue('white', 'gray.800')
   const color = useColorModeValue('black', 'gray.300')
 
