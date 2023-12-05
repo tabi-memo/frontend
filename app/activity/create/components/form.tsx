eb91e110a8fb0343328377dd07e62875029d9f99
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2023-12-05 12:34:06 -0800

    tailored the form to the figma design

--- app/activity/create/components/form.tsx
+++ app/activity/create/components/form.tsx
@@ -14,6 +14,7 @@ import {
   Flex
 } from '@chakra-ui/react'
 
+
 import { PrimaryButton } from '@/components/button'
 
 export const Form = () => {
@@ -36,22 +37,22 @@ export const Form = () => {
 
     const formData = new FormData(event.currentTarget)
     const title = formData.get('title') as string
-    const address = formData.get('address') as string
     const timeFrom = formData.get('timeFrom') as string
     const timeTo = formData.get('timeTo') as string
+    const address = formData.get('address') as string
     const url = formData.get('url') as string
     const memo = formData.get('memo') as string
     const cost = formData.get('cost') as string
 
     console.log({
       title,
-      address,
       timeFrom,
       timeTo,
+      address,
       url,
+      selectedImages,
       memo,
       cost,
-      selectedImages
     })
   }
 
@@ -61,86 +62,80 @@ export const Form = () => {
       pt={{ base: '40px', md: '40px' }}
       pb={{ base: '40px', md: '80px' }}
     >
-     <form onSubmit={handleSubmit}>
-      <FormControl isRequired>
-        <FormLabel>Title</FormLabel>
-        <Input name="title" type="text" placeholder="Enter title" />
-      </FormControl>
+      <form onSubmit={handleSubmit}>
+        <FormControl isRequired>
+          <FormLabel>Title</FormLabel>
+          <Input name="title" type="text" placeholder="Asakusa Temple" />
+        </FormControl>
 
-      <FormControl mt={{ base: '30px', md: '40px' }}>
-        <FormLabel>Address</FormLabel>
-        <Input name="address" type="text" placeholder="Enter address" />
-      </FormControl>
+        <FormControl mt={{ base: '30px', md: '40px' }}>
+          <FormLabel>Time From</FormLabel>
+          <Input name="timeFrom" type="time" />
+        </FormControl>
 
-      <FormControl mt={{ base: '30px', md: '40px' }}>
-        <FormLabel>Time From</FormLabel>
-        <Input name="timeFrom" type="time" />
-      </FormControl>
+        <FormControl mt={{ base: '30px', md: '40px' }}>
+          <FormLabel>Time To</FormLabel>
+          <Input name="timeTo" type="time"  />
+        </FormControl>
 
-      <FormControl mt={{ base: '30px', md: '40px' }}>
-        <FormLabel>Time To</FormLabel>
-        <Input name="timeTo" type="time" />
-      </FormControl>
+        <FormControl mt={{ base: '30px', md: '40px' }}>
+          <FormLabel>Address</FormLabel>
+          <Input name="address" type="text" placeholder="10-10 Shibuya, Tokyo, Japan" />
+        </FormControl>
 
-      <FormControl mt={{ base: '30px', md: '40px' }}>
-        <FormLabel>URL</FormLabel>
-        <Input name="url" type="url" placeholder="Enter URL" />
-      </FormControl>
+        <FormControl mt={{ base: '30px', md: '40px' }}>
+          <FormLabel>URL</FormLabel>
+          <Input name="url" type="url" placeholder="https://www.google.com" />
+        </FormControl>
 
-      <Text mt={{ base: '30px', md: '40px' }} fontWeight="medium">
-        Image
-      </Text>
-      <SimpleGrid
-        mt={{ base: '30px', md: '40px' }}
-        columns={{ base: 2, md: 3 }}
-        spacing={4}
-      >
-        {selectedImages.map((image, index) => (
-          <Image
-            key={index}
-            src={URL.createObjectURL(image)}
-            alt={`Selected Image ${image.name}`}
-            objectFit="cover"
-            width={{ base: '160px', md: '180px' }}
-            height={{ base: '106px', md: '120px' }}
-            margin="auto"
-            onClick={() => removeImage(index)}
-          />
-        ))}
-      </SimpleGrid>
-
-      <Box
-        {...getRootProps()}
-        textAlign="center"
-        mt={{ base: '30px', md: '40px' }}
-      >
-        <PrimaryButton variant="outline">
-          <input {...getInputProps()} />
-          Add Image
-        </PrimaryButton>
-      </Box>
-
-      <FormControl mt={{ base: '30px', md: '40px' }}>
-        <FormLabel>Memo</FormLabel>
-        <Textarea name="memo" placeholder="Enter memo" />
-      </FormControl>
-
-      <FormControl mt={{ base: '30px', md: '40px' }}>
-        <FormLabel>Cost</FormLabel>
-        <Input name="cost" type="number" placeholder="Enter cost" />
-      </FormControl>
-      
-
-      <Flex justifyContent="center">
-        <Button
+        <Text mt={{ base: '30px', md: '40px' }} fontWeight="medium">
+          Image
+        </Text>
+        <SimpleGrid
           mt={{ base: '30px', md: '40px' }}
-          colorScheme="teal"
-          type="submit"
-          margin="auto"
+          columns={{ base: 2, md: 3 }}
+          spacing={4}
         >
-          Create Activity
-        </Button>
-      </Flex>
+          {selectedImages.map((image, index) => (
+            <Image
+              key={index}
+              src={URL.createObjectURL(image)}
+              alt={`Selected Image ${image.name}`}
+              objectFit="cover"
+              width={{ base: '160px', md: '180px' }}
+              height={{ base: '106px', md: '120px' }}
+              margin="auto"
+              onClick={() => removeImage(index)}
+            />
+          ))}
+        </SimpleGrid>
+
+        <Box
+          {...getRootProps()}
+          textAlign="center"
+          mt={{ base: '30px', md: '40px' }}
+        >
+          <PrimaryButton variant="outline">
+            <input {...getInputProps()} />
+            Add Image
+          </PrimaryButton>
+        </Box>
+
+        <FormControl mt={{ base: '30px', md: '40px' }}>
+          <FormLabel>Memo</FormLabel>
+          <Textarea name="memo" placeholder="Type test here..." />
+        </FormControl>
+
+        <FormControl mt={{ base: '30px', md: '40px' }}>
+          <FormLabel>Cost</FormLabel>
+          <Input name="cost" type="number" placeholder="$200" />
+        </FormControl>
+
+        <Flex justifyContent="center" mt={{ base: '30px', md: '40px' }}>
+          <Button colorScheme="teal" type="submit" margin="auto">
+            Create Activity
+          </Button>
+        </Flex>
       </form>
     </Container>
   )
