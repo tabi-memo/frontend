37a46166bf686952471da5eb4e7ba4bb7a2805e0
Yo-mah-Ya <shanzhongziye@gmail.com>
2023-12-18 17:10:35 -0800

    Enable UUID coming from user session usable everywhere

--- app/(auth)/supabase/server.ts
+++ app/(auth)/supabase/server.ts
@@ -11,8 +11,15 @@ export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
         get(name: string) {
           return cookieStore.get(name)?.value
         },
-        set(name: string, value: string, options: CookieOptions) {
-          cookieStore.set({ name, value, ...options })
+        set(name: string, value: string) {
+          cookieStore.set({
+            name,
+            value,
+            httpOnly: true,
+            sameSite: 'lax',
+            maxAge: 60 * 60 * 24, // 24 hours
+            path: '/'
+          })
         },
         remove(name: string, options: CookieOptions) {
           cookieStore.set({ name, value: '', ...options })
