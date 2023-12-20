6a0a6631ad1d9867e08871beddfe263dcbfc027f
Yo-mah-Ya <shanzhongziye@gmail.com>
2023-12-20 09:42:06 -0800

    implemeneted signout

--- app/components/navigation/header.tsx
+++ app/components/navigation/header.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Link } from '@chakra-ui/next-js'
 import {
   Container,
@@ -9,11 +10,64 @@ import {
   MenuItem,
   IconButton,
   useColorMode,
-  useColorModeValue
+  useColorModeValue,
+  useToast,
+  useBoolean
 } from '@chakra-ui/react'
 import Image from 'next/image'
 import { FiMoon, FiSun } from 'react-icons/fi'
 import { MdAccountCircle, MdLogout } from 'react-icons/md'
+import { signOut } from '@/(auth)/signout/action'
+
+const LogOut = () => {
+  const toast = useToast()
+  const [isLoading, setIsLoading] = useBoolean()
+  isLoading
+  const [toastId, setToastId] = useState<ReturnType<typeof toast> | undefined>(
+    undefined
+  )
+
+  const signOutHandler = async () => {
+    try {
+      setIsLoading.on()
+      await signOut()
+    } catch (error) {
+      if (!toastId) {
+        const res = toast({
+          title: "We're sorry, but you failed to sign out.",
+          description: error instanceof Error ? error.message : 'unknown error',
+          status: 'error',
+          duration: 5000,
+          isClosable: true,
+          position: 'top',
+          onCloseComplete() {
+            setToastId(undefined)
+          }
+        })
+        setToastId(res)
+      }
+    } finally {
+      setIsLoading.off()
+    }
+  }
+
+  return (
+    <MenuItem
+      icon={<MdLogout size="20px" />}
+      bgColor="white"
+      _hover={{ bgColor: 'gray.100' }}
+      sx={{
+        svg: {
+          color: 'primary.700'
+        }
+      }}
+      onClick={signOutHandler}
+      isDisabled={isLoading}
+    >
+      Logout
+    </MenuItem>
+  )
+}
 
 export const Header = () => {
   const { colorMode, toggleColorMode } = useColorMode()
@@ -65,20 +119,7 @@ export const Header = () => {
                 >
                   Account Info
                 </MenuItem>
-                <MenuItem
-                  icon={<MdLogout size="20px" />}
-                  bgColor={bg}
-                  _hover={{
-                    bgColor: useColorModeValue('gray.100', 'gray.700')
-                  }}
-                  sx={{
-                    svg: {
-                      color: 'primary.700'
-                    }
-                  }}
-                >
-                  Logout
-                </MenuItem>
+                <LogOut />
               </MenuList>
             </Menu>
             <Box
