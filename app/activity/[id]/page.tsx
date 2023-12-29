b53e7be5ed8cfc698a9d28cf181752f43b156d4f
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2023-12-29 17:40:20 +0900

    fixed format

--- app/activity/[id]/page.tsx
+++ app/activity/[id]/page.tsx
@@ -5,15 +5,15 @@ import { Carousel } from '@/components/carousel'
 import { Link } from '@/components/link'
 import { Loading } from '@/components/loading'
 import { ActivityCard, ActivityInfo } from './components'
-import {
-  useActivityCollectionQuery
-} from '@generated/api'
+import { useActivityCollectionQuery } from '@generated/api'
 
-
-export default function ActivityDetails({ params }: { params: { id: number } }) {
+export default function ActivityDetails({
+  params
+}: {
+  params: { id: number }
+}) {
   const bg = useColorModeValue('white', 'gray.800')
   const color = useColorModeValue('black', 'gray.300')
-
 
   const { data, loading } = useActivityCollectionQuery({
     variables: {
@@ -21,18 +21,16 @@ export default function ActivityDetails({ params }: { params: { id: number } }) 
     }
   })
 
-
   const dummyUrls: string[] = [
     'https://images.unsplash.com/photo-1612852098516-55d01c75769a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
     'https://images.unsplash.com/photo-1627875764093-315831ac12f7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
     'https://images.unsplash.com/photo-1571432248690-7fd6980a1ae2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60'
   ]
 
-
   return (
     <>
       {loading || !data?.activityCollection ? (
-        <Flex minH="84vh" align="center" justify="center" >
+        <Flex minH="84vh" align="center" justify="center">
           <Loading />
         </Flex>
       ) : (
