1f27e844eabb1e931f41023196a62f148ee4b9bd
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2024-01-04 22:59:38 +0900

    test

--- app/activity/[id]/page.tsx
+++ app/activity/[id]/page.tsx
@@ -21,22 +21,19 @@ export default function ActivityDetails({
     }
   })
 
- 
   const dummyUrls: string[] = [
     'https://images.unsplash.com/photo-1612852098516-55d01c75769a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
     'https://images.unsplash.com/photo-1627875764093-315831ac12f7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
     'https://images.unsplash.com/photo-1571432248690-7fd6980a1ae2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60'
   ]
 
+  if (!loading && !data) throw new Error('No activity data found')
 
-   if (!loading && !data) throw new Error('No activity data found')
-
-   const activityData = data?.activityCollection?.edges[0]?.node
+  const activityData = data?.activityCollection?.edges[0]?.node
 
   return (
     <>
-    
-      { (!activityData || loading) ? (
+      {!activityData || loading ? (
         <Flex minH="84vh" align="center" justify="center">
           <Loading />
         </Flex>
@@ -47,7 +44,13 @@ export default function ActivityDetails({
             pt={{ base: '20px', md: '30px' }}
             pb={{ base: '40px', md: '80px' }}
           >
-            <ActivityHeader title={activityData.title} time_from={activityData.time_from} time_to={activityData?.time_to} address={activityData?.address} url={activityData?.url} />
+            <ActivityHeader
+              title={activityData.title}
+              time_from={activityData.time_from}
+              time_to={activityData?.time_to}
+              address={activityData?.address}
+              url={activityData?.url}
+            />
             <Carousel urls={dummyUrls} />
             <ActivityInfo memo={activityData?.memo} cost={activityData?.cost} />
             <Box mt="60px" display="flex" alignItems="center">
