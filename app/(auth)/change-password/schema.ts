9ca57eb4948541ec7f095f834811e8271e5cc400
Yo-mah-Ya <shanzhongziye@gmail.com>
2024-01-09 19:37:28 -0800

    fixed schema

--- app/(auth)/change-password/schema.ts
+++ app/(auth)/change-password/schema.ts
@@ -4,7 +4,7 @@ import * as z from 'zod'
 const changePasswordSchema = z
   .object({
     // No need for daring to notify users old password policy in terms of security enhancement.
-    oldPassword: z.string(),
+    oldPassword: z.string().min(1, { message: 'password is required' }),
     newPassword: z
       .string()
       .min(8, { message: 'Password must contain at least 8 character(s)' }),
@@ -15,6 +15,10 @@ const changePasswordSchema = z
   .refine((schema) => schema.newPassword === schema.confirmNewPassword, {
     path: ['confirmNewPassword'],
     message: 'Your password and confirmation password must match'
+  })
+  .refine((schema) => schema.oldPassword !== schema.newPassword, {
+    path: ['confirmNewPassword'],
+    message: 'Old password and new password must be different'
   })
 
 export type ChangePasswordSchema = z.infer<typeof changePasswordSchema>
