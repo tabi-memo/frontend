849929fd344adef9da36f056d1ec5b70671400e7
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2024-02-02 14:53:53 -0800

    sign up page ui

--- app/(auth)/signup/email/page.tsx
+++ app/(auth)/signup/email/page.tsx
@@ -2,19 +2,29 @@
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import {
+  AbsoluteCenter,
   Box,
-  Flex,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Center,
+  VStack,
   FormLabel,
   FormErrorMessage,
   FormControl,
-  Input,
   Heading,
   useToast,
-  useBoolean
+  useBoolean,
+  Text
 } from '@chakra-ui/react'
+import Image from 'next/image'
 import { signUp } from '@/(auth)/signup/email/action'
 import { signUpResolver, SignUpSchema } from '@/(auth)/signup/email/schema'
 import { PrimaryButton } from '@/components/button'
+import { InputForm } from '@/components/input'
+import { Link } from '@/components/link'
+import logo from 'public/logo/logo.png'
 
 export default function SignUp() {
   const toast = useToast()
@@ -56,46 +66,134 @@ export default function SignUp() {
   })
 
   return (
-    <>
-      <Heading>Welcome!</Heading>
-      <Heading>Create an Account</Heading>
-      <Box as="form" onSubmit={signUpHandler}>
-        <Flex flexDirection={'column'}>
-          <FormControl isInvalid={!!errors.name}>
-            <FormLabel>Name</FormLabel>
-            <Input {...register('name')} />
-            {errors.name && (
-              <FormErrorMessage>{errors.name.message}</FormErrorMessage>
-            )}
-          </FormControl>
-          <FormControl isInvalid={!!errors.email}>
-            <FormLabel>Email address</FormLabel>
-            <Input {...register('email')} />
-            {errors.email && (
-              <FormErrorMessage>{errors.email.message}</FormErrorMessage>
-            )}
-          </FormControl>
-          <FormControl isInvalid={!!errors.password}>
-            <FormLabel>Password</FormLabel>
-            <Input type="password" {...register('password')} />
-            {errors.password && (
-              <FormErrorMessage>{errors.password.message}</FormErrorMessage>
-            )}
-          </FormControl>
-          <FormControl isInvalid={!!errors.confirmationPassword}>
-            <FormLabel>Confirmation Password</FormLabel>
-            <Input type="password" {...register('confirmationPassword')} />
-            {errors.confirmationPassword && (
-              <FormErrorMessage>
-                {errors.confirmationPassword.message}
-              </FormErrorMessage>
-            )}
-          </FormControl>
-          <PrimaryButton isLoading={isLoading} type={'submit'}>
-            Sign Up
-          </PrimaryButton>
-        </Flex>
-      </Box>
-    </>
+    <Box
+      as="main"
+      minH={{ base: '100vh', md: '1024px' }}
+      w="100vw"
+      bg={{
+        base: 'white',
+        md: 'linear-gradient(242.77deg, #C4E6E6 1.15%, #085783 100%)'
+      }}
+      color="black"
+    >
+      <AbsoluteCenter>
+        <Card
+          bg="#F8F8F8"
+          boxShadow={{ base: '0', md: '0 4px 16px rgba(0, 0, 0, 0.1)' }}
+          maxW={{ base: '100%', lg: 'container.sm' }}
+          py={{ base: '40px', md: '40px' }}
+          px={{ base: '16px', md: '80px' }}
+        >
+          <CardHeader w="100%" p="0">
+            <Center w="100%">
+              <Image src={logo} width={144} height={40} alt="Tabi Memo" />
+            </Center>
+            <VStack
+              w="100%"
+              mt={{ base: '28px', md: '40px' }}
+              align={{ base: 'center', md: 'start' }}
+            >
+              <Heading
+                as="h1"
+                fontSize={{ base: '2xl', md: '4xl' }}
+                color="gray.700"
+              >
+                Create your account
+              </Heading>
+              <Heading
+                as="h2"
+                fontSize="sm"
+                color="gray.700"
+                fontWeight="regular"
+              >
+                It{"'s"} quick and easy
+              </Heading>
+            </VStack>
+          </CardHeader>
+          <CardBody
+            as="form"
+            onSubmit={signUpHandler}
+            mt={{ base: '38px', md: '42px' }}
+            gap={{ base: '30px', md: '40px' }}
+            p="0%"
+            w={{ base: '80vw', md: 'unset' }}
+            maxW="500px"
+          >
+            <VStack gap="24px">
+              <FormControl isInvalid={!!errors.name}>
+                <FormLabel>Name</FormLabel>
+                <InputForm {...register('name')} placeholder="John Smith" />
+                {errors.name && (
+                  <FormErrorMessage>{errors.name.message}</FormErrorMessage>
+                )}
+              </FormControl>
+              <FormControl isInvalid={!!errors.email}>
+                <FormLabel>Email</FormLabel>
+                <InputForm
+                  {...register('email')}
+                  placeholder="tabimemo@example.com"
+                  minW={{ base: '300px', md: '380px' }}
+                />
+                {errors.email && (
+                  <FormErrorMessage>{errors.email.message}</FormErrorMessage>
+                )}
+              </FormControl>
+              <FormControl isInvalid={!!errors.password}>
+                <FormLabel>Password</FormLabel>
+                <InputForm
+                  type="password"
+                  {...register('password')}
+                  placeholder="password"
+                />
+                {errors.password && (
+                  <FormErrorMessage>{errors.password.message}</FormErrorMessage>
+                )}
+              </FormControl>
+              <FormControl isInvalid={!!errors.confirmationPassword}>
+                <FormLabel>Confirm Password</FormLabel>
+                <InputForm
+                  type="password"
+                  {...register('confirmationPassword')}
+                  placeholder="password"
+                />
+                {errors.confirmationPassword && (
+                  <FormErrorMessage>
+                    {errors.confirmationPassword.message}
+                  </FormErrorMessage>
+                )}
+              </FormControl>
+            </VStack>
+            <PrimaryButton
+              isLoading={isLoading}
+              type={'submit'}
+              w="100%"
+              mt="30px"
+            >
+              Sign Up
+            </PrimaryButton>
+          </CardBody>
+          <CardFooter
+            mt={{ base: '38px', md: '42px' }}
+            p="0"
+            gap={{ base: '30px', md: '40px' }}
+          >
+            <VStack w="100%">
+              <Text fontSize="sm" color="gray.600">
+                Already have an account?
+              </Text>
+              <Link
+                hasUnderLine={true}
+                fontSize="sm"
+                color="primary.800"
+                fontWeight="semibold"
+                href="/signIn/email"
+              >
+                Sign In
+              </Link>
+            </VStack>
+          </CardFooter>
+        </Card>
+      </AbsoluteCenter>
+    </Box>
   )
 }
