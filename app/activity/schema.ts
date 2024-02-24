ab9868bfea193f8021ae7e78f3a30d0fa74c8852
samuraikun <tazitaziawawa@gmail.com>
2024-02-23 17:23:00 -0800

    fix for lint format.

--- app/activity/schema.ts
+++ app/activity/schema.ts
@@ -1,23 +1,24 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as z from 'zod'
 
-const activitySchema = z.object({
-  title: z.string().min(1, { message: 'Please enter a title' }),
-  timeFrom: z.date({
-    required_error: 'Please select a date from',
-    invalid_type_error: "That's not a date"
-  }),
-  timeTo: z.date().nullable(),
-  address: z.string().min(0).max(50).nullable(),
-  url: z.union([z.string().url().nullish(), z.literal('')]),
-  memo: z.string().min(0).max(300).nullable(),
-  cost: z.string().nullable(),
-  costUnit: z.string().nullable(),
-  uploadedFileUrls: z.array(z.string()).optional(),
-  newFiles: z.array(z.instanceof(File)).optional()
-})
+const activitySchema = z
+  .object({
+    title: z.string().min(1, { message: 'Please enter a title' }),
+    timeFrom: z.date({
+      required_error: 'Please select a date from',
+      invalid_type_error: "That's not a date"
+    }),
+    timeTo: z.date().nullable(),
+    address: z.string().min(0).max(50).nullable(),
+    url: z.union([z.string().url().nullish(), z.literal('')]),
+    memo: z.string().min(0).max(300).nullable(),
+    cost: z.string().nullable(),
+    costUnit: z.string().nullable(),
+    uploadedFileUrls: z.array(z.string()).optional(),
+    newFiles: z.array(z.instanceof(File)).optional()
+  })
   .refine(
-    args => {
+    (args) => {
       if (!args.timeTo) return true
 
       const { timeFrom, timeTo } = args
