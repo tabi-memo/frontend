21fdf2b99da4f44833a1ca6c2718c238d3b039f6
samuraikun <tazitaziawawa@gmail.com>
2024-03-22 11:24:36 -0700

    fix next.config.js to display uploaded images on production

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
         hostname: '127.0.0.1',
         port: '54321',
         pathname: '/storage/v1/object/public/**'
+      },
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_SUPABASE_URL,
+        pathname: '/storage/v1/object/public/**'
       }
     ]
   }
